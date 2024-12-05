7c7ee6df28696ecc4e18d0e13907f652cf96cbc7
ahmadreza <ahmadreza.an2001@gmail.com>
2024-12-05 18:51:17 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,866 @@
+// var userage = Number(prompt('سن کاربر اول:'))
+
+// var ageme = Number(prompt('سن کاربر دوم:'))
+
+
+// var result = userage + ageme
+
+// alert(result)
+
+
+// var name='ahmadreza'
+
+// var lastname='ansari'
+
+// alert(name +'\n' +lastname )
+
+
+
+// alert('my name is /ahmadreza/ansari')
+
+
+
+// var num1=12
+
+// var num2=4
+
+// var num3=3
+
+// if(num2 * num3===num1){
+
+
+//     alert('بله برابر است')
+
+
+// }else{
+
+//     alert('برابر نیست')
+// }
+
+
+
+
+// var userage=Number(prompt('سن خود را وارد کنید:'))
+
+// if(userage % 2===0){
+//     alert('عدد زوج است')
+// }else{
+//     alert('عدد فرد است')
+// }
+
+
+
+// var num1 = Number(prompt('عدد اول'))
+
+// var num2 = Number(prompt('عدد دوم'))
+
+// var num3 = Number(prompt('عدد سوم'))
+
+
+// var result = (num1 + num2 + num3) / 3
+
+// alert(result)
+
+
+// var userage=Number(prompt('age:'))
+
+// if(userage < 18){
+
+//     alert('شما مجاز به ورود به پنل نیستید')
+// }else{
+//     alert("تبریک شما مجاز به ورود به پنل هستید")
+// }
+
+
+
+// var birthdayuser=Number(prompt('تاریخ تولد خود را وارد کنید:'))
+
+// if(birthdayuser < 1403){  
+
+
+//     var result=1403 - birthdayuser
+
+//     alert(result)
+
+//     }else{
+//         alert('تاریخ تولد وارد شده معتبر نیست')
+//     }
+
+
+// var min = Number(prompt('دقیقه را وارد کنید'))
+
+
+// var result = min / 60
+
+
+
+// if (min < 0 ||  isNaN(min)) {
+
+//     alert('مقدار نامعتبر است')
+// }else{
+
+//     alert(result)
+
+// }
+
+
+// if(){
+
+// }else if(){
+
+// }else
+
+
+// var userage=Number(prompt('inter age:'))
+
+// var jens=prompt("jens")
+
+// if(userage < 18 || jens==='مونث'){
+
+//     alert('خطا')
+// }else{
+//     alert('خوش امدید')
+// }
+
+
+// var num1=3
+// var num2=4
+// var num3=12
+
+// if(num1 * num2===num3){
+//     alert('افرین')
+// }else{
+//     alert('نه اشتباه کردی')
+// }
+
+// num1 * num2===num3?alert('افرین'):alert('نه اشتباه کردی')
+
+
+
+// var num1 = 3
+// var num2 = 4
+// var num3 = num1 * num2
+
+
+// switch (num3) {
+
+//     case 10:
+//         alert('نام 3 برابر 10 است')
+//         break
+
+
+//     case 11:
+//         alert('نام 3 برابر 11 است')
+//         break
+
+//     case 12:
+//         case 15:
+//         alert('نام 3 برابر 12 یا 15 است')
+//         break
+
+//     case 13:
+//         alert('نام 3 برابر 13 است')
+//         break
+
+//     case 14:
+//         alert('نام 3 برابر14 است')
+//         break
+
+//     default:
+//         alert('نام 3 برابر هیچکدوم نیست')
+// }
+
+
+// var moadel = Number(prompt('moadel:'))
+
+
+//  if (moadel > 0 && moadel < 20) {
+
+
+// switch (moadel) {
+
+
+//   case 20:
+//   case 19:
+//   case 18:
+//     alert("A")
+//     break
+
+//   case 17:
+//   case 16:
+//   case 15:
+//     alert("B")
+//     break
+
+//   case 14:
+//   case 13:
+//   case 12:
+//     alert("C")
+//     break
+
+//   default:
+//     alert("مشروط شدی :(")
+
+
+
+
+
+// }
+
+
+// }
+
+
+// function value() {
+
+
+
+//     var num1 = 8
+//     var num2 = 2
+//     var result = num1 + num2
+//     alert(result)
+// }
+// value()
+
+
+
+// var show = function value() {
+
+
+
+//     var num1 = 8
+//     var num2 = 2
+//     var result = num1 + num2
+//     alert(result)
+// }
+// value()
+
+
+
+
+
+// switch (browser) {
+//     case 'Edge':
+//       alert("You've got the Edge!");
+//       break;
+
+//     case 'Chrome':
+//     case 'Firefox':
+//     case 'Safari':
+//     case 'Opera':
+//       alert('Okay we support these browsers too');
+//       break;
+
+//     default:
+//       alert('We hope that this page looks ok!');
+//   }
+
+
+
+// switch (browser) {
+//   case 'Edge':
+//     alert("You've got the Edge!");
+//     break;
+
+//   case 'Chrome':
+//   case 'Firefox':
+//   case 'Safari':
+//   case 'Opera':
+//     alert('Okay we support these browsers too');
+//     break;
+
+//   default:
+//     alert('We hope that this page looks ok!');
+// }
+
+
+
+
+// function value() {
+
+
+
+//     var num1 = 8
+//     var num2 = 2
+//     var result = num1 + num2
+//     alert(result)
+// }
+// value()
+
+
+
+// var show = function value() {
+
+
+
+//     var num1 = 8
+//     var num2 = 2
+//     var result = num1 + num2
+//     alert(result)
+// }
+// value()
+
+
+// function value(num1) {
+
+//     if (num1 % 2 === 0) {
+
+//         alert(num1)
+
+//     }
+
+
+// }
+
+// value(num1 = Number(prompt('num1:')))
+
+
+
+
+// function value3(num1,num2,num3){
+
+//    var result=(num1 + num2 + num3) / 3
+//     alert(result)
+
+
+// }
+
+// value3(
+// num1=Number(prompt('adad1:')),
+// num2=Number(prompt('adad2:')),
+// num3=Number(prompt('adad3:'))
+// )
+
+
+
+
+// var num1 = Number(prompt('adad1:'))
+// var num2 = Number(prompt('adad2:'))
+// var num3 = Number(prompt('adad3:'))
+
+// function value1(num1, num2, num3) {
+
+//     result =num1 + num2 + num3
+
+//     return result
+
+
+// }
+
+// var show=value1(num1,num2,num3)
+
+// alert(show)
+
+
+
+// var username='ahmadreza'
+
+// alert(username[2])
+// alert(username.length)
+
+
+
+
+// var username = prompt('username:')
+
+// var pass = (prompt('pass:'))
+
+// if (username.length == 3 && pass.length == 8) {
+
+//     alert('ثبت نام شما یا موفقیت انجام شد')
+
+// } else {
+//     alert('اشتباه کردی')
+// }
+
+
+// var text='ahmadreza'
+
+// console.log(text.charAt(0))
+// console.log(text.charCodeAt(0))
+// console.log(text.concat())
+// console.log(text.trim())
+// console.log(text.toLowerCase())
+// console.log(text.toUpperCase())
+// console.log(text.search('r'))
+// console.log(text.indexOf('r'))
+
+
+
+// var username = prompt('username:')
+
+// if (username.toLocaleLowerCase() === "ali") {
+
+//     alert("لاگین موفیت امیز بود")
+// }else{
+//     alert('شما مجوز ورود به سایت نگرفتید')
+// }
+
+// console.log(username.toLowerCase())
+
+// var text='ahmadreza ansari'
+
+// console.log(text.slice(7,17))
+// console.log(text.substring())
+
+
+// console.log(Math.PI)
+//  console.log(Math.pow(2,3))
+// console.log(Math.sqrt(16))
+// console.log(Math.abs(-10))
+// console.log(Math.min(20,40,65,78,3))
+// console.log(Math.min(20,40,65,78,3,125))
+// console.log(Math.floor(12.8))
+// console.log(Math.trunc(12.8))
+// console.log(Math.round(12.1))
+// console.log(Math.ceil(12.1))
+// console.log(Math.random())
+
+
+
+// console.log(Math.random() * 50000)
+
+// var ran = Math.random() * 50000
+// var result = Math.floor(ran)
+
+// if (result > 10000 && result < 100000) {
+
+//     alert(result)
+
+// }
+
+
+
+// var num1 = 0
+
+
+
+// var num2 =   num1 ++ 
+
+
+// console.log(num1)
+// console.log(num2)
+
+
+
+
+// for (var i = 0; i <= 100; i++) {
+
+//     if(i % 2==0){
+//         alert(i)
+//     }
+
+// }
+
+
+// var num1 = 0
+
+// for (var i = 0; i < 5; i++) {
+
+//     num1 += Number(prompt('adad:'))
+
+
+// }
+// alert(num1)
+
+
+
+
+
+
+
+
+
+
+// var number = Number(prompt('adad:'))
+
+
+// var sum = 0
+
+// for (var i =1; i < 100; i+=2) {
+
+
+//     sum += i
+
+// }
+// alert(sum)
+
+
+// var number = Number(prompt('adad:'))
+
+
+
+//var num = Number(prompt('adad:'))
+
+// var nn=0
+
+// for (var i = num; num / i;) {
+
+//     alert(nn)
+
+// }
+
+
+
+
+// var usernumber = Number(prompt("usernumber:"))
+
+// var sum = 0
+
+// for (var i = 0; i <= usernumber; i++) {
+
+//     if (usernumber % i == 0) {
+
+//         sum += usernumber % i == 0
+//         //alert(i)
+//     }
+// }
+
+// alert(sum)
+
+
+
+
+
+
+//عغلغعلع
+
+// var usernumber = Number(prompt('user number:'))
+// var sum = 0
+
+// for (; (usernumber / 10) != 0; ) {
+
+
+//     sum += usernumber % 10
+//     usernumber = Math.floor(usernumber / 10)
+// }
+
+
+// alert(sum)
+
+
+//تاتاتختحخنجح
+
+
+//for (; (usernumber / 10) != 0; ) {
+
+
+//sum++
+//  usernumber = Math.floor(usernumber / 10)
+//}
+
+
+//alert(sum)
+
+// var number = 5
+
+// var sum = 1
+
+// for (var i = 1; i <= 6; i++) {
+
+//     sum *=i 
+//     alert(sum)
+// }
+
+
+// var mylist = ['apple', 'ahmadreza', 'ali']
+
+
+
+// for (var i = 0; i < mylist.length; i++) {
+
+
+
+//     alert(mylist[i])
+
+// }
+
+
+
+// for (var i = 0; ((i <= 10) / 2) != 0; i++) {
+
+//     alert(i)
+
+// }
+
+
+// let i = 0
+// var sum = 0
+
+// while (i < 3) {
+
+//     i++
+//     sum += (Number(prompt("number:"))) / 3
+
+// }
+// alert(sum)
+
+
+
+// var usernumber = Number(prompt("number:"))
+
+// var sum = 0
+
+// while ((usernumber / 10) != 0) {
+
+//     sum += usernumber % 10
+//     usernumber = Math.floor(usernumber / 10)
+
+// }
+
+// alert(sum)
+
+
+// var usernumber = Number(prompt("number:"))
+
+// var sum = 0
+
+// while ((usernumber / 10) != 0) {
+//     sum++
+//     usernumber = Math.floor(usernumber / 10)
+
+// }
+
+// alert(sum)
+
+
+
+// var firstnumber = Number(prompt('first number:'))
+// var secondnumber = Number(prompt('second number:'))
+
+
+
+// if (firstnumber % 2 === 0) {
+
+//     while (secondnumber > firstnumber) {
+
+//         alert(firstnumber += 2)
+//     }
+
+// }
+
+// else {
+//     firstnumber++
+//     while (secondnumber > firstnumber) {
+//         alert(firstnumber += 2)
+//     }
+
+// }
+
+
+
+
+// var number = 0
+// var sum = 0
+// var count = 0
+
+// while (number != -1) {
+//     sum += number
+//     number = Number(prompt('number:  \n  ahmadreza'))
+
+//     if (number != -1) {
+
+//         count++
+//         result=number / count
+
+//     }
+
+
+// }
+
+
+//alert(result)
+
+// var num = 0
+// var sum = 0
+
+// var n = Number(prompt("number:"))
+
+// var sum = 0
+// var i = 0
+
+
+// while (i <= n) {
+//     i++
+//     sum+=i
+//     alert(sum)
+
+
+// }
+
+
+//var firstnumber = Number(prompt('firstnumber:'))
+//var secondnumber = Number(prompt("secondnumber:"))
+
+// var sum = 1
+// var i = 1
+
+// for (var i = 0; secondnumber > i; i++) {
+//     i++
+//     sum = firstnumber ** secondnumber
+//     alert(sum)
+
+// }
+
+
+// while (secondnumber > i) {
+//     i++
+//     sum = firstnumber ** secondnumber
+
+// }
+
+// alert(sum)
+
+
+// var product = 1
+// var i = 1
+
+
+// var firstnumber = Number(prompt('firstnumber:'))
+
+// do {
+//     i++
+//     product *= firstnumber
+//     alert(product)
+// } while (firstnumber > i)
+
+
+// var student = ['ali', 'ahmadreza', 'qoli', 20]
+
+
+// alert(student.length)
+// console.log(length.student[2])
+
+
+
+//var sum = 0
+//var i = secondnumber = Number(prompt("second number:"))
+
+//do {
+
+//sum += i++
+
+
+//} while (firstnumber >= i)
+
+//alert(sum)
+
+
+//for (var i = 0; firstnumber > secondnumber; i++) {
+
+//    sum += secondnumber++
+
+
+//alert(sum)
+
+//}
+
+
+//while (firstnumber >= i) {
+
+
+//    sum += secondnumber++
+
+
+
+
+
+//var firstnumber = Number(prompt("first number:"))
+//var secondnumber = Number(prompt("second number:"))
+
+
+// var sum = 0
+// var sad = 100
+
+
+// while (firstnumber >= secondnumber) {
+//     secondnumber++
+//     sum *= secondnumber
+
+// }
+// alert(sum)
+
+
+
+// var i=0
+
+// do {
+//     i+=sum++
+//     alert(i)
+
+// } while(sum <= sad)
+
+
+// var scores=[11,19,20,16,17]
+
+// scores.shift()
+// console.log(scores)
+
+// scores.unshift(10,18,14,13)
+
+// console.log(scores)
+
+
+// var usernumber = 0
+// var scores = []
+// var average = 0
+
+// while (usernumber != -1) {
+//     usernumber = Number(prompt('user number:'))
+
+//     if (usernumber != -1) {
+//         scores.push(usernumber)
+//         average += usernumber
+//         var result = average / scores.length
+
+//     }
+// }
+
+// var result = average / scores.length
+
+// alert(scores)
+// alert(result)
+
+
+
+// console.log(scores)
+
+// scores[5]=13
+
+// scores.push(14,17,10)
+
+// console.log(scores)
+
+// scores.pop()
+
+// console.log(scores)
+
+
+
+// var users = ['ahmadreza', 'ali', 'hassan', 'qoli', 'arman']
+
+
+// var user = {
+//     name: 'ahmadreza'
+//     , family: 'ansari'
+//     , age: '23'
+//     , phon: 3034
+// }
+
+// alert(user.age)
+// alert(user['phon'])
+
+
+
+//var big = [{id:1, name: 'ahmadreza', family: 'ansari', age: 23 },
+//{id:2, name: 'hamid', family: 'hamidi', age: 18 }
+//]
+
+
+//index 1
+//alert(big[1].age)
+
+
+
+
+

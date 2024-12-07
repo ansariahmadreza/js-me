57c662fb03152ddc4edac1518555f61476fcd7bc
ahmadreza <ahmadreza.an2001@gmail.com>
2024-12-07 19:56:05 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -864,3 +864,62 @@
 
 
 
+var users = [{ id: 1, name: 'ahmadreza', family: 'ansari', age: 23 },
+{ id: 2, name: 'hamid', family: 'hamidi', age: 18 },]
+
+var username = prompt('user name:')
+var userfamily = prompt('user family:')
+var useremail = prompt('user email:')
+var userage = Number(prompt('user age:'))
+
+
+
+if (username.length < 3 || username.length > 10) {
+
+    alert('لطفا بین 3 تا 10 کاراکتر وارد کنید')
+
+} else if (userfamily.length < 3 || userfamily.length > 15) {
+
+    alert('لطفا بین 3 تا 10 کاراکتر وارد کنید')
+} else if (userage > 100) {
+    alert('لطفا عدد زیر100 وارد کنید')
+}
+
+else {
+    var newuser = {
+        id: 4,
+        name: username,
+        family: userfamily,
+        age: userage,
+        email: useremail,
+
+
+
+    }
+}
+
+console.log(newuser)
+
+
+
+
+
+
+var users = [{ id: 1, name: 'ahmadreza', family: 'ansari', age: 23 ,email:'ahmadreza.an2001@gmail.com'},
+    { id: 2, name: 'hamid', family: 'hamidi', age: 18,email:'ahmadreza.an2001@outlook.com' },'ali','arman','hasan'
+    ]
+    
+    users.forEach(function(users){
+        console.log(users)
+    })
+    
+    
+    // var scores=[20,18,11,12,5,4]
+    
+    // scores.forEach(function(number){
+    
+    
+    // console.log(number * 2)
+    
+    // })
+    

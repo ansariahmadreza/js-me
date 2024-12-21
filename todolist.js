5392777b158fdda8c552781f78104acc560cc4c9
ahmadreza <ahmadreza.an2001@gmail.com>
2024-12-21 15:10:53 +0330

    Add files via upload

--- todolist.js
+++ todolist.js
@@ -5,15 +5,10 @@ var todolist = [{ id: 1, name: 'emtahanat', isstat: true },
 
 var todolistuser = prompt('1.add todo\n2.remove todo\n3.do status')
 
-
+////////////////////////////////////////////////////////////////////////////
 if (todolistuser === '1') {
 
     var addtodouser = prompt('add todo user:')
-
-
-
-
-
     var isall = todolist.some(function (items) {
 
         return items.name === addtodouser
@@ -36,6 +31,7 @@ if (todolistuser === '1') {
         console.log(todolist)
     }
 }
+///////////////////////////////////////////////
 
 else if (todolistuser === '2') {
 
@@ -59,7 +55,7 @@ else if (todolistuser === '2') {
         alert('محصول وجود ندارد')
     }
 
-
+    ///////////////////////////////////////////////////////////
 
 } else if (todolistuser === '3') {
 
@@ -80,4 +76,74 @@ else if (todolistuser === '2') {
 } else {
 
     alert('لطفا گزینه صحیح را وارد کنید')
+}
+
+
+var todolistuser = prompt('1.add todo\n2.remove todo\n3.do status')
+
+
+
+//switch case
+
+
+
+switch (todolistuser) {
+
+
+    case '1':
+
+        var addtodouser = prompt('add todo user:')
+        var isall = todolist.some(function (items) {
+
+            return items.name === addtodouser
+
+        })
+
+        console.log(isall)
+
+        if (isall === true) {
+
+            alert('لطفا وضعیت غیر تکراری ثبت کن')
+
+        } else if (!isall) {
+            var newtodo = [{
+                id: todolist.length + 1,
+                name: addtodouser,
+                stat: 'off',
+            }]
+            todolist.push(newtodo)
+            console.log(todolist)
+        }
+
+        break;
+
+    case '2':
+
+
+        addtodouser = prompt('inter todo:')
+
+        var isindex = todolist.findIndex(function (items) {
+
+            return items.name === addtodouser
+
+        })
+
+        console.log(isindex)
+
+        if (isindex != -1) {
+
+            todolist.splice(isindex, 1)
+            console.log(todolist)
+
+
+        } else {
+            alert('محصول وجود ندارد')
+        }
+        break;
+
+
+
+
+
+
 }

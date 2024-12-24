d6ad5af427f8694a0c81469de76e2490df1b471d
ahmadreza <ahmadreza.an2001@gmail.com>
2024-12-24 20:17:58 +0330

    Add files via upload

---
+++ set.js
@@ -0,0 +1,58 @@
+var picture = prompt(' 1.true\n2.false')
+
+
+
+
+
+if (picture === '1') {
+
+
+  setTimeout(function (picture) {
+
+    alert('افرین بر شما برای وقت شناسی و بارگذاری به موقع عکس')
+
+  }, 5000,1)
+
+
+
+
+
+} else if (picture === '2') {
+
+
+  setTimeout(function (picture) {
+
+    alert('متاسفانه عکس مربوطه بارگذاری نشده است')
+
+  }, 5000,2)
+
+
+
+
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// setInterval(function (num,num1) {
+
+//   alert(num + num1)
+
+// }, 5000,10,10)
